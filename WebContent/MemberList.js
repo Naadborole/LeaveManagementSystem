5ff34b7db30f5b52602e78165b7b12b36ffc5331
abr2711 <abr63499@gmail.com>
2021-05-17 12:50:39 +0530

    minor changes

--- WebContent/MemberList.js
+++ WebContent/MemberList.js
@@ -30,8 +30,8 @@ function insertList()
         $("#memberEmail").html(arrData[i].Email);
         $("#Leaves").html("Total Leaves available : " + arrData[i].TotalLeaves);*/
         var div1 = document.createElement('div');
-        div1.className = 'card card-pad h-auto w-75 mx-auto';
-        div1.style.borderColor = "#e00";   
+        div1.className = 'card card-pad h-auto w-75 mx-auto card-shadow';
+        //div1.style.borderColor = "#e00";   
         div1.innerHTML = '<div class="row-flex"><div class="col-md-8"><h4 class="card-title" id="memberName">'+arrData[i].FirstName+" "+arrData[i].LastName+'</h4><h5 class="card-subtitle mb-2 text-muted" id="memberEmail">'+arrData[i].Email+'</h5> </div><div class="col-md-4 my-auto"><h4 class="card-title" id="Leaves">Total Leaves available :  '+ arrData[i].TotalLeaves+'</h4></div></div>';
         $(".col-md").append(div1);
         $(".col-md").append('<br/>')
